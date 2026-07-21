127e2789248cf23048840fa9fdf0e209806dab41
smart03 <olekelechi03@gmail.com>
2026-07-22 00:20:53 +0100

    kc

---
+++ script.js
@@ -0,0 +1,61 @@
+// question 1
+let age = 20;
+
+if (age >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible to vote");
+}
+
+// question 2
+let score = 76;
+
+if (score >= 70 && score <= 100) {
+  console.log("A");
+} else if (score >= 60 && score <= 69) {
+  console.log("B");
+} else if (score >= 50 && score <= 59) {
+  console.log("C");
+} else {
+  console.log("F");
+}
+
+// question 3
+let num1 = 40;
+let num2 = 65;
+
+if (num1 > num2) {
+  console.log(num1 + " is the largest");
+} else if (num2 > num1) {
+  console.log(num2 + " is the largest");
+} else {
+  console.log("Both numbers are equal");
+}
+
+// question 4
+let userName = "admin";
+let passsWord = "1234";
+
+if (userName === "admin" && passsWord === "1234") {
+  console.log("Login Successful");
+} else {
+  console.log("Invalid Username or Password");
+}
+
+// question 5
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+// question 6
+for (let i = 2; i <= 50; i += 2) {
+  console.log(i);
+}
+
+// question 7
+for (let i = 1; i <= 25; i += 2) {
+  console.log(i);
+}
+
+// question 8
+
